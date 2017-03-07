bdfd996cf566af8000a1c46ba5f6f231f5f4418a
wm1993 <1174637194@qq.com>
2017-03-07 10:29:29 +0800

    修改本地图片获取方式：AsyncTask实现

--- app/src/main/java/com/example/admin/networkimageloader/ListImgsFragment.java
+++ app/src/main/java/com/example/admin/networkimageloader/ListImgsFragment.java
@@ -4,9 +4,9 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -22,7 +22,6 @@ import com.example.admin.networkimageloader.utils.Images;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -45,70 +44,7 @@ public class ListImgsFragment extends Fragment
 	{
 		super.onCreate(savedInstanceState);
 		mImageLoader = ImageLoader.getInstance(3, ImageLoader.Type.LIFO);
-		mImageLoader.clearDiskCache(getActivity());
-		mHandler = new Handler() {
-			@Override
-			public void handleMessage(Message msg) {
-				super.handleMessage(msg);
-				int size = mGruopMap.size();
-				Iterator iter = mGruopMap.entrySet().iterator();
-				while (iter.hasNext()) {
-					Map.Entry entry = (Map.Entry) iter.next();
-					String key = (String) entry.getKey();
-					List<String> val = (List<String>) entry.getValue();
-					mlist.addAll(val);
-					Log.d("手机图片key", key);
-					for (String str : val) {
-						Log.d("手机图片val", str);
-					}
-
-				}
-				mlist.clear();
-				mlist.addAll(Arrays.asList(mUrlStrs));
-				ListImgItemAdaper adapter= (ListImgItemAdaper) mGridView.getAdapter();adapter.notifyDataSetChanged();
-			}
-		};
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-				ContentResolver mContentResolver = getActivity().getContentResolver();
-
-				//只查询jpeg和png的图片
-				Cursor mCursor = mContentResolver.query(mImageUri, null,
-						MediaStore.Images.Media.MIME_TYPE + "=? or "
-								+ MediaStore.Images.Media.MIME_TYPE + "=?",
-						new String[]{"image/jpeg", "image/png"}, MediaStore.Images.Media.DATE_MODIFIED);
-
-				if (mCursor == null) {
-					return;
-				}
-
-				while (mCursor.moveToNext()) {
-					//获取图片的路径
-					String path = mCursor.getString(mCursor
-							.getColumnIndex(MediaStore.Images.Media.DATA));
-
-					//获取该图片的父路径名
-					String parentName = new File(path).getParentFile().getName();
-
-
-					//根据父路径名将图片放入到mGruopMap中
-					if (!mGruopMap.containsKey(parentName)) {
-						List<String> chileList = new ArrayList<String>();
-						chileList.add(path);
-						mGruopMap.put(parentName, chileList);
-					} else {
-						mGruopMap.get(parentName).add(path);
-					}
-				}
-
-				//通知Handler扫描图片完成
-				mHandler.sendEmptyMessage(123);
-				mCursor.close();
-			}
-		}).start();
-
+         new FindLocalImageFile().execute(getActivity());
 	}
 
 	@Override
@@ -158,10 +94,71 @@ public class ListImgsFragment extends Fragment
 			ImageView imageview = (ImageView) convertView
 					.findViewById(R.id.id_img);
 			imageview.setImageResource(R.drawable.pictures_no);
-			mImageLoader.loadImage(getItem(position), imageview,true);
+			mImageLoader.loadImage(getItem(position), imageview);
 			return convertView;
 		}
 
 	}
+  class FindLocalImageFile extends AsyncTask<Context,Integer,List<String>>{
 
+	  @Override
+	  protected List<String> doInBackground(Context... params) {
+		  Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+		  ContentResolver mContentResolver = params[0].getContentResolver();
+
+		  //只查询jpeg和png的图片
+		  Cursor mCursor = mContentResolver.query(mImageUri, null,
+				  MediaStore.Images.Media.MIME_TYPE + "=? or "
+						  + MediaStore.Images.Media.MIME_TYPE + "=?",
+				  new String[]{"image/jpeg", "image/png"}, MediaStore.Images.Media.DATE_MODIFIED);
+
+		  if (mCursor == null) {
+			  return  null;
+		  }
+
+		  while (mCursor.moveToNext()) {
+			  //获取图片的路径
+			  String path = mCursor.getString(mCursor
+					  .getColumnIndex(MediaStore.Images.Media.DATA));
+
+			  //获取该图片的父路径名
+			  String parentName = new File(path).getParentFile().getName();
+
+
+			  //根据父路径名将图片放入到mGruopMap中
+			  if (!mGruopMap.containsKey(parentName)) {
+				  List<String> chileList = new ArrayList<String>();
+				  chileList.add(path);
+				  mGruopMap.put(parentName, chileList);
+			  } else {
+				  mGruopMap.get(parentName).add(path);
+			  }
+		  }
+		  List<String> mlist=new ArrayList<>();
+		  Iterator iter = mGruopMap.entrySet().iterator();
+		  while (iter.hasNext()) {
+			  Map.Entry entry = (Map.Entry) iter.next();
+			  String key = (String) entry.getKey();
+			  List<String> val = (List<String>) entry.getValue();
+			  mlist.addAll(val);
+		  }
+
+		  //通知Handler扫描图片完成
+		  mCursor.close();
+		  return mlist;
+	  }
+
+	  @Override
+	  protected void onProgressUpdate(Integer... values) {
+		  super.onProgressUpdate(values);
+	  }
+
+	  @Override
+	  protected void onPostExecute(List<String> strings) {
+		  super.onPostExecute(strings);
+		  mlist.clear();
+		  mlist.addAll(strings);
+		  ListImgItemAdaper adapter= (ListImgItemAdaper) mGridView.getAdapter();adapter.notifyDataSetChanged();
+	  }
+  }
 }
